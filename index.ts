24f39be92f9acd31da57a64a9f853aa968caa33c
Igor Savin <iselwin@gmail.com>
2023-05-27 16:02:47 +0300

    Add exports

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { BullFeeder } from 'lib/BullFeeder'
+export type { JobOptions, BullFeederConfig } from 'lib/BullFeeder'
+
+export { RabbitmqTaskInputSource } from 'lib/amqp/RabbitmqTaskInputSource'
+export type { MessageTransformer, TaskInputMessage, RabbitmqTaskInputSourceConfig } from 'lib/amqp/RabbitmqTaskInputSource'
