476f39a7942de3d641c201c0c24a056158d7efa0
Igor Savin <iselwin@gmail.com>
2023-05-27 14:41:36 +0300

    Make it possible to pass additional bull config

--- lib/BullFeeder.ts
+++ lib/BullFeeder.ts
@@ -1,9 +1,11 @@
 import { Queue } from 'bullmq'
+import type { QueueOptions } from 'bullmq/'
 import type { Redis } from 'ioredis'
 
 export type BullFeederConfig = {
   queueName: string
   redis: Redis
+  queueOptions?: QueueOptions
 }
 
 export type JobOptions = {
@@ -14,7 +16,10 @@ export class BullFeeder<TaskPayload = Record<string, unknown>> {
   private readonly queue: Queue
 
   constructor(config: BullFeederConfig) {
-    this.queue = new Queue(config.queueName, { connection: config.redis })
+    this.queue = new Queue(config.queueName, {
+      ...config.queueOptions,
+      connection: config.redis,
+    })
   }
 
   createTask(jobOptions: JobOptions, taskPayload: TaskPayload): Promise<unknown> {
